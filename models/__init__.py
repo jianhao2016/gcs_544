297c15fa4f3d1a0759dbb8a35a9ef9fd59c27914
jianhao <jianhao2@illinois.edu>
2017-12-05 01:28:22 -0600

    3rd commit. Add __init__.pu

---
+++ models/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017 qizai <jianhao2@illinois.edu>
+#
+# Distributed under terms of the MIT license.
+
+"""
+This is a init file for google cloud.
+"""
