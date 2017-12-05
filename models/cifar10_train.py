7729f45b2509240fc9b281a4c965c6c66773d6a2
jianhao <jianhao2@illinois.edu>
2017-12-05 02:47:25 -0600

    jianhao. fixed the batch norm error in r1.2

--- models/cifar10_train.py
+++ models/cifar10_train.py
@@ -47,10 +47,15 @@ def batch_norm_relu(inputs, is_training, data_format):
   """Performs a batch normalization followed by a ReLU."""
   # We set fused=True for a significant performance boost. See
   # https://www.tensorflow.org/performance/performance_guide#common_fused_ops
+  # # another error cause by r1.2. Unfortunately.
+  # inputs = tf.layers.batch_normalization(
+  #     inputs=inputs, axis=1 if data_format == 'channels_first' else 3,
+  #     momentum=_BATCH_NORM_DECAY, epsilon=_BATCH_NORM_EPSILON, center=True,
+  #     scale=True, training=is_training, fused=True)
   inputs = tf.layers.batch_normalization(
       inputs=inputs, axis=1 if data_format == 'channels_first' else 3,
       momentum=_BATCH_NORM_DECAY, epsilon=_BATCH_NORM_EPSILON, center=True,
-      scale=True, training=is_training, fused=True)
+      scale=True, training=is_training)
   inputs = tf.nn.relu(inputs)
   return inputs
 
